96eb7b942674aa9a62b304d87a19fa0af555cbea
Paul Lesieur <plesieur@jpkeefehs.org>
2022-11-06 14:15:35 -0500

    Add project files.

---
+++ Debug 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Debug_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phoneticAlphabet =  {  "Alpha", "Beta", "Charlie", "Delta", "Echo", "Foxtrot", "Golf",
+                                            "Hotel","India","Juliett","Kilo","Lima","Mike","November",
+                                            "Oscar","Papa","Quebec","Romeo","Sierra","Tango",
+                                            "Uniform","Victor","Whiskey","X-Ray","Yankee","Zulu"};
+
+            for (int i = 1; i <= phoneticAlphabet.Length; i++)
+            {
+                Console.WriteLine(phoneticAlphabet[i]);
+            }
+        }
+    }
+}
