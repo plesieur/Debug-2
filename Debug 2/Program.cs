0234f386e800a8a6d2d95a90c78b589b8223536a
Paul Lesieur <plesieur@jpkeefehs.org>
2022-11-07 10:39:25 -0500

    Added 2D arrary to debug

--- Debug 2/Program.cs
+++ Debug 2/Program.cs
@@ -11,9 +11,24 @@ namespace Debug_2
                                             "Oscar","Papa","Quebec","Romeo","Sierra","Tango",
                                             "Uniform","Victor","Whiskey","X-Ray","Yankee","Zulu"};
 
+            int[,] dataTable = new int[3, 5]
+            {
+                {0, 1, 2, 4, 5},
+                {10, 11, 12, 14, 15},
+                {20, 21, 22, 24, 25}
+            };
+
             for (int i = 1; i <= phoneticAlphabet.Length; i++)
             {
                 Console.WriteLine(phoneticAlphabet[i]);
+            }
+            for (int i=0;  i < 5; i++)
+            {
+                for (int j=0; j < 3; j++)
+                {
+                    Console.Write("{0} ",dataTable[i,j]);
+                }
+                Console.WriteLine();
             }
         }
     }
